67bfa4e7cd8b05d39c3dbdea295f41f0b80dc36c
imllz <imzll0218@163.com>
2018-07-29 20:31:24 +0800

    银行卡号格式化

---
+++ source/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-form>
+      <el-input
+        :value="bankAccount"
+        @keydown.native="keydowmHandle"
+        @input="filterBankAccount"></el-input>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Bank',
+  data () {
+    return {
+      bankAccount: ''
+    }
+  },
+  methods: {
+    keydowmHandle (e) {
+      console.log(e)
+      const keyCodes = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
+      if (!keyCodes.includes(e.keyCode)) e.preventDefault()
+    },
+    filterBankAccount (value) {
+      console.log(value)
+      // if ()
+      // const last = value.charAt(value.length - 1)
+      // if (!/\d/.test(last)) el
+      this.bankAccount = value.replace(/\s/g, '').replace(/(\d{4})(?=\d)/g, '$1 ')
+    }
+  }
+}
+</script>
